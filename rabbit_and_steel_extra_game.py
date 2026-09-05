cef3bc82673281e0115ee72ba3972aebbe6ed236
Sogeki <kurt.savio+git@hotmail.fr>
2026-09-05 13:51:40 +0200

    Add files via upload

---
+++ rabbit_and_steel_extra_game.py
@@ -0,0 +1,252 @@
+from __future__ import annotations
+
+import functools
+
+from typing import List
+
+from dataclasses import dataclass
+
+from Options import OptionSet, Toggle, DefaultOnToggle
+
+from ..game import Game
+from ..game_objective_template import GameObjectiveTemplate
+
+from ..enums import KeymastersKeepGamePlatforms
+
+
+@dataclass
+class RabbitAndSteelExtraArchipelagoOptions:
+    rabbit_and_steel_extra_include_extra: RabbitAndSteelExtraIncludeExtra
+    rabbit_and_steel_extra_unlocked_characters: RabbitAndSteelExtraUnlockedCharacters
+    rabbit_and_steel_extra_allow_lunar: RabbitAndSteelExtraAllowLunar
+
+
+class RabbitAndSteelExtraGame(Game):
+    name = "Rabbit & Steel (Extra)"
+    platform = KeymastersKeepGamePlatforms.PC
+
+    is_adult_only_or_unrated = False
+
+    options_cls = RabbitAndSteelExtraArchipelagoOptions
+
+    def optional_game_constraint_templates(self) -> List[GameObjectiveTemplate]:
+        return list()
+
+    def game_objective_templates(self) -> List[GameObjectiveTemplate]:
+        templates: List[GameObjectiveTemplate] = [
+            GameObjectiveTemplate(
+                label="Complete the STAGE on DIFF difficulty",
+                data={"STAGE": (self.stages, 1), "DIFF": (self.difficulties, 1)},
+                is_time_consuming=False,
+                is_difficult=False,
+                weight=3
+            ),
+            GameObjectiveTemplate(
+                label="Complete the STAGE as the CHARACTER",
+                data={"STAGE": (self.stages, 1), "CHARACTER": (self.characters, 1)},
+                is_time_consuming=False,
+                is_difficult=False,
+                weight=3
+            ),
+            GameObjectiveTemplate(
+                label="Complete the STAGE on DIFF difficulty as the CHARACTER",
+                data={"STAGE": (self.stages, 1), "DIFF": (self.difficulties, 1), "CHARACTER": (self.characters, 1)},
+                is_time_consuming=False,
+                is_difficult=False,
+                weight=2
+            ),
+            GameObjectiveTemplate(
+                label="Scale the Moonlit Pinnacle and defeat Shira, Rabbit of the Moon on DIFF difficulty",
+                data={"DIFF": (self.difficulties, 1)},
+                is_time_consuming=True,
+                is_difficult=False, # Depends on optional factors, very easy on Easy
+                weight=2
+            ),
+            GameObjectiveTemplate(
+                label="Scale the Moonlit Pinnacle and defeat Shira, Rabbit of the Moon as the CHARACTER",
+                data={"CHARACTER": (self.characters, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=2
+            ),
+            GameObjectiveTemplate(
+                label="Scale the Moonlit Pinnacle and defeat Shira, Rabbit of the Moon on DIFF difficulty as the CHARACTER",
+                data={"DIFF": (self.difficulties, 1), "CHARACTER": (self.characters, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=2
+            ),
+            GameObjectiveTemplate(
+                # I'm pretty sure this is impossible on Lunar...
+                label="Complete the STAGE while skipping on all loot rolls on DIFF difficulty",
+                data={"STAGE": (self.stages, 1), "DIFF": (self.base_difficulties, 1)},
+                is_time_consuming=False,
+                is_difficult=True,
+                weight=2
+            ),
+            GameObjectiveTemplate(
+                label="Complete 5 stages while playing as the CHARACTER",
+                data={"CHARACTER": (self.characters, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=3
+            ),
+            GameObjectiveTemplate(
+                label="Complete 5 stages on DIFF difficulty",
+                data={"DIFF": (self.difficulties, 1)},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=3
+            ),
+            GameObjectiveTemplate(
+                label="Pet Asha, the Shopkeeper",
+                data={},
+                is_time_consuming=True,
+                is_difficult=False,
+                weight=1
+            ),
+        ]
+        
+        if self.archipelago_options.rabbit_and_steel_extra_include_extra.value:
+            templates.extend([
+                GameObjectiveTemplate(
+                    label="Scale the Reflecting Pool and defeat The Witch of Sorrowed Heart on DIFF difficulty",
+                    data={"DIFF": (self.difficulties, 1)},
+                    is_time_consuming=True,
+                    is_difficult=False, # Depends on optional factors, very easy on Easy
+                    weight=2
+                ),
+                GameObjectiveTemplate(
+                    label="Scale the Reflecting Pool and defeat The Witch of Sorrowed Heart as the CHARACTER",
+                    data={"CHARACTER": (self.characters, 1)},
+                    is_time_consuming=True,
+                    is_difficult=False,
+                    weight=2
+                ),
+                GameObjectiveTemplate(
+                    label="Scale the Reflecting Pool and defeat The Witch of Sorrowed Heart on DIFF difficulty as the CHARACTER",
+                    data={"DIFF": (self.difficulties, 1), "CHARACTER": (self.characters, 1)},
+                    is_time_consuming=True,
+                    is_difficult=False,
+                    weight=2
+                ),
+                GameObjectiveTemplate(
+                    label="Pet Tassha, the Snowfur Wolf",
+                    data={},
+                    is_time_consuming=True,
+                    is_difficult=False,
+                    weight=1
+                ),
+                GameObjectiveTemplate(
+                    label="Try to pet Saya, the Archon Crow",
+                    data={},
+                    is_time_consuming=True,
+                    is_difficult=False,
+                    weight=1
+                )
+            ])
+        
+        return templates
+
+    @functools.cached_property
+    def characters_base(self):
+        return [
+            "Wizard Rabbit",
+            "Assassin Rabbit",
+            "Heavyblade Rabbit",
+            "Dancer Rabbit",
+            "Druid Rabbit"
+        ]
+        
+    @functools.cached_property
+    def characters_extra(self):
+        return [
+            "Hammermaid Rabbit",
+            "Pyromancer Rabbit",
+            "Grenadier Rabbit",
+            "Shadow Rabbit"
+        ]
+
+    def characters(self):
+        characters = self.characters_base[:]
+        characters.extend(self.archipelago_options.rabbit_and_steel_extra_unlocked_characters.value)
+        
+        if not self.archipelago_options.rabbit_and_steel_extra_include_extra:
+            characters.extend(self.archipelago_options.rabbit_and_steel_extra_unlocked_characters.value)
+            for extra_character in self.characters_extra:
+                while extra_character in characters:
+                    characters.remove(extra_character)
+
+        return characters
+
+    @staticmethod
+    def base_difficulties():
+        return [
+            "Cute",
+            "Normal",
+            "Hard"
+        ]
+
+    def difficulties(self):
+        difficulties = self.base_difficulties()
+        
+        if self.archipelago_options.rabbit_and_steel_extra_allow_lunar:
+            difficulties.append("Lunar")
+            
+        return difficulties
+
+    @functools.cached_property
+    def base_stages(self):
+        return [
+            "Kingdom Outskirts",
+            "Scholar's Nest",
+            "King's Arsenal",
+            "Red Darkhouse",
+            "Churchmouse Streets",
+            "Emerald Lakeside",
+            # Leaving out the Pale Keep for time constraints
+        ]
+        
+    @functools.cached_property
+    def extra_stages(self):
+        return [
+            "Crack in the Geode",
+            "Darkhouse Depths",
+            "Subterra Sanctum",
+            "Atelier Aurum"
+        ]
+
+    def stages(self):
+        stages = self.base_stages[:]
+        
+        if self.archipelago_options.rabbit_and_steel_extra_include_extra:
+            stages.extend(self.extra_stages)
+            
+        return stages
+
+
+class RabbitAndSteelExtraUnlockedCharacters(OptionSet):
+    """Indicates which unlockable characters in Rabbit & Steel the player has obtained"""
+    display_name = "Rabbit & Steel Unlocked Characters"
+    valid_keys = {
+        "Spellsword Rabbit",
+        "Sniper Rabbit",
+        "Bruiser Rabbit",
+        "Defender Rabbit",
+        "Ancient Rabbit",
+        "Hammermaid Rabbit",
+        "Pyromancer Rabbit",
+        "Grenadier Rabbit",
+        "Shadow Rabbit"
+    }
+
+    default = valid_keys
+
+
+class RabbitAndSteelExtraAllowLunar(Toggle):
+    """Whether the Lunar difficulty should be an option for objectives in Rabbit & Steel"""
+    display_name = "Rabbit & Steel Allow Lunar Difficulty"
+    
+class RabbitAndSteelExtraIncludeExtra(DefaultOnToggle):
+    """Whether the extra stages and characters should be options for objectives in Rabbit & Steel"""
+    display_name = "Rabbit & Steel Include Extra Mode"
